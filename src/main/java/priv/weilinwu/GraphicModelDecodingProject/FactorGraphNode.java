2e3b204648090808fb6e86ebb814c19cbb19394b
wwwmonsterlam <wuweilin.william@qq.com>
2017-10-18 12:37:36 -0300

    some update

--- src/main/java/priv/weilinwu/GraphicModelDecodingProject/FactorGraphNode.java
+++ src/main/java/priv/weilinwu/GraphicModelDecodingProject/FactorGraphNode.java
@@ -47,8 +47,36 @@ public class FactorGraphNode {
 	public void passMessagesWtihSumProductAlgo() {
 		// the messages sent from a function node and a variable node follow different rules
 		if(isFunctionNode()) {
-			
+			// this is a function node
+			// update message on each outgoing edge
+			FactorGraphEdge targetEdge = this.outgoingEdge;
+			while(targetEdge != null) {
+				// there are 3 incoming edges 
+				// (except for the one corresponding to the target edge)
+				// get the messages on these three incoming edges				
+				double[][] incomingMessages = new double[3][];
+				FactorGraphEdge tempIncomingEdge = this.incomingEdge;
+				int i = 0;
+				while(tempIncomingEdge != null) {
+					if(!targetEdge.getHeadNode().equals(tempIncomingEdge.getTailNode())) {
+						incomingMessages[i++] = tempIncomingEdge.getMessage();
+					}
+					tempIncomingEdge = tempIncomingEdge.getNextIncomingEdge();
+				}
+				
+				double[] tempMessage = new double[2];
+				// when the variable in the head node of the target edge equals to 0
+				// there are 8 combination of the 3 incoming edge messages, i.e., 000, 001, ..., 111
+				// the function of the this function node is specified in the report
+				
+				
+				// update the message on the target edge
+				targetEdge.setMessage(tempMessage);
+				
+				targetEdge = targetEdge.getNextOutgoingEdge();
+			}			
 		} else {
+			// this is a variable node
 			// update message on each outgoing edge
 			FactorGraphEdge targetEdge = this.outgoingEdge;
 			while(targetEdge != null) {
